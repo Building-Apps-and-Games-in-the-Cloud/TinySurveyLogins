76fdf5b14160ad630df3d4111c604ea5edd90c40
Rob Miles <rob@robmiles.com>
2023-03-09 13:16:24 +0000

    Upgraded webtoken version

--- helpers/usermanager.mjs
+++ helpers/usermanager.mjs
@@ -6,29 +6,28 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-class UserManager{
+class UserManager {
 
     constructor() {
     }
 
-async init()
-{
-    const adminUser = await SurveyUsers.findOne({ email: process.env.INITIAL_ADMIN_USERNAME });
+    async init() {
+        const adminUser = await SurveyUsers.findOne({ email: process.env.INITIAL_ADMIN_USERNAME });
 
-    if (adminUser == null) {
-        console.log(`  Admin user not registered`);
-        const hashedPassword = await bcrypt.hash(process.env.INITIAL_ADMIN_PASSWORD, 10);
-        const user = new SurveyUsers(
-            {
-                name: "Admin",
-                password: hashedPassword,
-                role: 'admin',
-                email: process.env.INITIAL_ADMIN_USERNAME
-            });
-        await user.save();
-        console.log("  Admin user successfully registered");
+        if (!adminUser) {
+            console.log(`  Admin user not registered`);
+            const hashedPassword = await bcrypt.hash(process.env.INITIAL_ADMIN_PASSWORD, 10);
+            const user = new SurveyUsers(
+                {
+                    name: "Admin",
+                    password: hashedPassword,
+                    role: 'admin',
+                    email: process.env.INITIAL_ADMIN_USERNAME
+                });
+            await user.save();
+            console.log("  Admin user successfully registered");
+        }
     }
-}
 }
 
 export { UserManager as UserManager };
